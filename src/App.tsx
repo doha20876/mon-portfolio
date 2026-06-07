7394bff29485abf9b9afdf8ae6abdd57621cf3d1
unknown <doha>
2026-06-07 21:31:15 +0100

    projects section

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,45 @@ function App() {
         </div>
 
       </div>
+<section className="mt-24 max-w-5xl w-full">
 
+  <h2 className="text-4xl font-bold text-center mb-12 text-purple-500">
+    Mes Projets
+  </h2>
+
+  <div className="grid md:grid-cols-2 gap-8">
+
+    <div className="bg-zinc-900 p-6 rounded-2xl border border-zinc-800">
+      <h3 className="text-2xl font-semibold mb-4">
+        Site E-commerce
+      </h3>
+
+      <p className="text-gray-400 mb-4">
+        Boutique moderne développée avec React et Tailwind CSS.
+      </p>
+
+      <button className="bg-purple-600 px-5 py-2 rounded-xl">
+        Voir Projet
+      </button>
+    </div>
+
+    <div className="bg-zinc-900 p-6 rounded-2xl border border-zinc-800">
+      <h3 className="text-2xl font-semibold mb-4">
+        Application IA
+      </h3>
+
+      <p className="text-gray-400 mb-4">
+        Projet intelligent utilisant l’intelligence artificielle.
+      </p>
+
+      <button className="bg-purple-600 px-5 py-2 rounded-xl">
+        Voir Projet
+      </button>
+    </div>
+
+  </div>
+
+</section>
     </div>
   )
 }
